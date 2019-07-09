348268574931133cfbde5206b8da862c3b554a1f
David <davidmenhur@gmail.com>
2019-07-09 21:44:39 +0200

    .

--- bioinfo/code/plot_rama.py
+++ bioinfo/code/plot_rama.py
@@ -74,11 +74,11 @@ plt.xlabel(r'$\phi$')
 plt.ylabel(r'$\psi$')
 
 plt.colorbar(label='RSA')
-ax.xaxis.set_major_formatter(FuncFormatter(lambda val, pos: '{:.0g}$\pi$'.format(val / np.pi) if val != 0 else '0'))
-ax.xaxis.set_major_locator(MultipleLocator(base=np.pi))
+ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
 
-ax.yaxis.set_major_formatter(FuncFormatter(lambda val, pos: '{:.0g}$\pi$'.format(val / np.pi) if val != 0 else '0'))
-ax.yaxis.set_major_locator(MultipleLocator(base=np.pi))
+ax.yaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+ax.yaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
 
 plt.tight_layout()
 plt.savefig('../figures/transfer_learning.pdf')

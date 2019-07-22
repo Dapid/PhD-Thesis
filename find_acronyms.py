ed0d7c4a35448fd4e6ecb130ed5f5762a00ba350
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2019-07-22 17:00:52 +0200

    .

--- find_acronyms.py
+++ find_acronyms.py
@@ -10,12 +10,14 @@ for fname in tex:
     f = open(fname)
     for line in f:
         line = line.split('%', 1)[0].strip()
-        words = re.split(' |,|\.|-|:|;|\(|\)|\[|\]|{|}|\\\\|~', line)
+        words = re.split(' |,|\.|-|:|;|\(|\)|\[|\]|{|}|~', line)
         for w in words:
             if '$' in w: continue
             if len(w) < 3: continue
             if len(w) > 30: continue
             if w.startswith('\\'):continue
+            if w == 'CONFOLD':
+                print(line)
             if sum(1 for x in w if x in uppercase) > 2: 
                 acronyms.add(w)
 
